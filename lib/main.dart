a32de2f72b528d4f75136a7fc45fc328f6a23bf4
ogabek0909 <norpulatovogabek42@gmail.com>
2023-06-20 00:15:14 +0000

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:bloc_counter/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(BlocProvider(
+    create: (context) => CounterCubit(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,14 +25,23 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  BlocProvider.of<CounterCubit>(context).increment();
+                },
                 icon: const Icon(Icons.add),
               ),
-              const Text('Number'),
+              BlocBuilder<CounterCubit, CounterState>(
+                builder: (context, state) {
+                  return Text(state.number.toString());
+                },
+              ),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  BlocProvider.of<CounterCubit>(context).decrement();
+                },
                 icon: const Icon(Icons.remove),
               ),
             ],
